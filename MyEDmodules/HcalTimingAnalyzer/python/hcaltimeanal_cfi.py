d597b8469c74b22fe722339c349dd4ffe972912a
dudero <>
2009-04-09 22:02:51 +0000

    config tweaks for code rewrite

--- MyEDmodules/HcalTimingAnalyzer/python/hcaltimeanal_cfi.py
+++ MyEDmodules/HcalTimingAnalyzer/python/hcaltimeanal_cfi.py
@@ -1,18 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
 myan = cms.EDAnalyzer('HcalTimingAnalyzer',
-    eventNumbers    = cms.vint32(),
-    hbheDigiLabel   = cms.untracked.InputTag("hcalDigis"),
-    hbheRechitLabel = cms.untracked.InputTag("hbhereco"),
-    hfRechitLabel   = cms.untracked.InputTag("hfreco"),
-    metLabel        = cms.untracked.InputTag("metNoHF"),
-                      
+    eventDataPset = cms.untracked.PSet(
+      hbheDigiLabel   = cms.untracked.InputTag("hcalDigis"),
+      hbheRechitLabel = cms.untracked.InputTag("hbhereco"),
+      hfRechitLabel   = cms.untracked.InputTag("hfreco"),
+      simHitLabel     = cms.untracked.InputTag("g4SimHits","HcalHits"),
+      metLabel        = cms.untracked.InputTag("metNoHF"),
+      twrLabel        = cms.untracked.InputTag("towerMaker")
+    ),
+
+    tgtTwrId     = cms.vint32(),
+    eventNumbers = cms.vint32(),
+
     minHitGeVHB  = cms.double(0.5),
     minHitGeVHE  = cms.double(0.6),  # = avg (0.5 single width, 0.7 double)
     minHitGeVHO  = cms.double(0.5),
     minHitGeVHF1 = cms.double(1.2),
     minHitGeVHF2 = cms.double(1.8),
-                      
-    recHitEscaleMinGeV = cms.double(-50.0),
-    recHitEscaleMaxGeV = cms.double(100.0)
+
+    recHitEscaleMinGeV = cms.double(-0.5),
+    recHitEscaleMaxGeV = cms.double(400.5)
 )
