ae62c0c13d03f63c5a08c8f4b0f181882224e534
dudero <>
2009-07-27 16:08:52 +0000

    added FEDrawData and HcalTBTriggerData

--- MyEDmodules/MyAnalUtilities/interface/myEventData.hh
+++ MyEDmodules/MyAnalUtilities/interface/myEventData.hh
@@ -16,7 +16,7 @@
 //
 // Original Author:  Phillip Russell DUDERO
 //         Created:  Tue Sep  9 13:11:09 CEST 2008
-// $Id: myEventData.hh,v 1.2 2009/05/18 01:14:15 dudero Exp $
+// $Id: myEventData.hh,v 1.3 2009/07/03 11:29:14 dudero Exp $
 //
 //
 
@@ -27,12 +27,18 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include "DataFormats/FEDRawData/interface/FEDRawData.h"
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DataFormats/FEDRawData/interface/FEDNumbering.h"
+#include "DataFormats/HcalDigi/interface/HcalCalibrationEventTypes.h"
 #include "DataFormats/HcalDigi/interface/HcalLaserDigi.h"
 #include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/METReco/interface/CaloMET.h"
 #include "DataFormats/METReco/interface/CaloMETCollection.h"
 #include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
+#include "TBDataFormats/HcalTBObjects/interface/HcalTBTriggerData.h"
+
 #include "SimDataFormats/CaloHit/interface/PCaloHit.h"
 #include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
 
@@ -47,25 +53,29 @@ public:
 
   void get(const edm::Event&, const edm::EventSetup&);
 
-  inline edm::Handle<HcalLaserDigi>          laserdigi(void)   const { return laserdigi_;   }
-  inline edm::Handle<HBHEDigiCollection>     hbhedigis(void)   const { return hbhedigis_;   }
-  inline edm::Handle<HBHERecHitCollection>   hbherechits(void) const { return hbherechits_; }
-  inline edm::Handle<HFRecHitCollection>     hfrechits(void)   const { return hfrechits_;   }
-  inline edm::Handle<HFDigiCollection>       hfdigis(void)     const { return hfdigis_;     }
-  inline edm::Handle<HORecHitCollection>     horechits(void)   const { return horechits_;   }
-  inline edm::Handle<HODigiCollection>       hodigis(void)     const { return hodigis_;     }
-  inline edm::Handle<CaloTowerCollection>    towers(void)      const { return towers_;      }
-  inline edm::Handle<CaloMETCollection>      recmet(void)      const { return recmet_;      }
-  inline edm::Handle<edm::PCaloHitContainer> simhits(void)     const { return hsimhits_;    }
+  inline edm::Handle<HcalTBTriggerData>      hcaltbtrigdata(void) const { return hcaltbtrigdata_; }
+  inline edm::Handle<FEDRawDataCollection>   fedrawdata(void)    const { return fedrawdata_;  }
+  inline edm::Handle<HcalLaserDigi>          laserdigi(void)     const { return laserdigi_;   }
+  inline edm::Handle<HBHEDigiCollection>     hbhedigis(void)     const { return hbhedigis_;   }
+  inline edm::Handle<HBHERecHitCollection>   hbherechits(void)   const { return hbherechits_; }
+  inline edm::Handle<HFRecHitCollection>     hfrechits(void)     const { return hfrechits_;   }
+  inline edm::Handle<HFDigiCollection>       hfdigis(void)       const { return hfdigis_;     }
+  inline edm::Handle<HORecHitCollection>     horechits(void)     const { return horechits_;   }
+  inline edm::Handle<HODigiCollection>       hodigis(void)       const { return hodigis_;     }
+  inline edm::Handle<CaloTowerCollection>    towers(void)        const { return towers_;      }
+  inline edm::Handle<CaloMETCollection>      recmet(void)        const { return recmet_;      }
+  inline edm::Handle<edm::PCaloHitContainer> simhits(void)       const { return hsimhits_;    }
 
-  inline uint32_t                            runNumber(void)   const { return runn_; }
-  inline uint32_t                            evtNumber(void)   const { return evtn_; }
+  inline uint32_t                            runNumber(void)     const { return runn_; }
+  inline uint32_t                            evtNumber(void)     const { return evtn_; }
 
 private:
 
   // ----------member data ---------------------------
+  bool localHCALrun_;
 
   // configurable parameter:
+  edm::InputTag      fedRawDataTag_;
   edm::InputTag      laserDigiTag_;
   edm::InputTag      hbheRechitTag_;
   edm::InputTag      hbheDigiTag_;
@@ -82,6 +92,8 @@ private:
   uint32_t runn_;
   uint32_t evtn_;
 
+  edm::Handle<HcalTBTriggerData>      hcaltbtrigdata_;
+  edm::Handle<FEDRawDataCollection>   fedrawdata_;
   edm::Handle<edm::PCaloHitContainer> hsimhits_;
   edm::Handle<HcalLaserDigi>          laserdigi_;
   edm::Handle<HBHERecHitCollection>   hbherechits_;
