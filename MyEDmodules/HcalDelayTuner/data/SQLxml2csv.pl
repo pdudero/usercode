3a1671d326a7cd1369b4621ce68db02cd249fdb8
dudero <>
2009-11-12 22:40:17 +0000

    old and new delay settings extracted from XML bricks, and the scripts to do the extracting

---
+++ MyEDmodules/HcalDelayTuner/data/SQLxml2csv.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+#
+
+if (@ARGV < 1) {
+#    print "Usage: $0 brick*.xml\n";
+#    exit(1);
+    processFile("-");
+} else {
+    foreach $file (@ARGV) {
+	processFile($file);
+    }
+}
+
+sub processFile {
+    my ($inp)=@_;
+    #print $inp,"\n";
+    open(INFILE,$inp);
+    while(<INFILE>) {
+	if (/Parameter name=\'RBX\'.*>(.*)<\/Parameter>/) {
+	    $rbx=$1;
+	}
+	elsif (/Data.*rm=\"(\d{1})\" card=\"(\d{1})\" qie=\"(\d{1})\">(\d+)<\/Data/) {
+	    $rm=$1;
+	    $card=$2;
+	    $qie=$3;
+	    $delay=$4;
+	    print "$rbx\t$rm\t$card\t$qie\t$delay\n";
+	}
+    }
+    close(INFILE);
+}
