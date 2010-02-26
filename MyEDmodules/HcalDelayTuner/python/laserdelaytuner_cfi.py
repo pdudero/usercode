6d4870d92bd8d238ff636acb5075b9632bd1753d
dudero <>
2010-02-26 23:44:16 +0000

    *** empty log message ***

--- MyEDmodules/HcalDelayTuner/python/laserdelaytuner_cfi.py
+++ MyEDmodules/HcalDelayTuner/python/laserdelaytuner_cfi.py
@@ -1,14 +1,32 @@
 import FWCore.ParameterSet.Config as cms
 
-tundelayser = cms.EDAnalyzer('LaserDelayTuner',
-                             
+hbdelayser = cms.EDAnalyzer('LaserTimingAnalyzer',
+    subdet            = cms.untracked.string("HB"),
+    runDescription    = cms.untracked.string(""),
+    globalTimeOffset  = cms.double(0.0),
+    globalRecHitFlagMask = cms.int32(0),
+    minHitGeV          = cms.double(25),
+    timeWindowMinNS    = cms.double(-100.0),
+    timeWindowMaxNS    = cms.double(100.0),
+    recHitEscaleMinGeV = cms.double(-0.5),
+    recHitEscaleMaxGeV = cms.double(400.5),
+    recHitTscaleNbins  = cms.int32(201),
+    recHitTscaleMinNs  = cms.double(-100.5),
+    recHitTscaleMaxNs  = cms.double(100.5),
+    maxEventNum2plot   = cms.int32(1),
+    CorrectionsList    = cms.VPSet(),
+    detIds2mask        = cms.vint32(),
+    normalizeDigis     = cms.bool(False),
+    doPerChannel       = cms.bool(True),
+                            
     eventDataPset = cms.untracked.PSet(
-      fedRawDataLabel = cms.untracked.InputTag("source"),
+      fedRawDataLabel = cms.untracked.InputTag(""),
+      tbTrigDataLabel = cms.untracked.InputTag("tbunpack"),
       laserDigiLabel  = cms.untracked.InputTag("hcalLaserReco"),
       hbheDigiLabel   = cms.untracked.InputTag("hcalDigis"),
       hbheRechitLabel = cms.untracked.InputTag("hbhereco"),
-      hfRechitLabel   = cms.untracked.InputTag("hfreco"),
-      hoRechitLabel   = cms.untracked.InputTag("horeco"),
+      hfRechitLabel   = cms.untracked.InputTag(""),
+      hoRechitLabel   = cms.untracked.InputTag(""),
       verbose         = cms.untracked.bool(False)
     ),
                              
@@ -19,40 +37,17 @@ tundelayser = cms.EDAnalyzer('LaserDelayTuner',
       TimeModCeiling = cms.untracked.int32(9999)
     ),
 
-    HBpars = cms.PSet(
-      minHitGeV          = cms.double(25), # from Luis' program
-      recHitEscaleMinGeV = cms.double(-0.5),
-      recHitEscaleMaxGeV = cms.double(400.5),
-      recHitTscaleNbins  = cms.int32(201),
-      recHitTscaleMinNs  = cms.double(-100.5),
-      recHitTscaleMaxNs  = cms.double(100.5)
-    ),
-
-    HEpars = cms.PSet(
-      minHitGeV          = cms.double(25), # from Luis' program
-      recHitEscaleMinGeV = cms.double(-0.5),
-      recHitEscaleMaxGeV = cms.double(400.5),
-      recHitTscaleNbins  = cms.int32(201),
-      recHitTscaleMinNs  = cms.double(-100.5),
-      recHitTscaleMaxNs  = cms.double(100.5)
-    ),
-
-    HOpars = cms.PSet(
-      minHitGeV          = cms.double(25), # from Luis' program
-      recHitEscaleMinGeV = cms.double(-0.5),
-      recHitEscaleMaxGeV = cms.double(400.5),
-      recHitTscaleNbins  = cms.int32(201),
-      recHitTscaleMinNs  = cms.double(-100.5),
-      recHitTscaleMaxNs  = cms.double(100.5)
-    ),
-    HFpars = cms.PSet(
-      minHitGeV          = cms.double(25), # from Luis' program
-      recHitEscaleMinGeV = cms.double(-0.5),
-      recHitEscaleMaxGeV = cms.double(400.5),
-      recHitTscaleNbins  = cms.int32(201),
-      recHitTscaleMinNs  = cms.double(-100.5),
-      recHitTscaleMaxNs  = cms.double(100.5)
-    ),
-
     bottomfeeder = cms.untracked.int32(0xbadf00d)
 )
+
+hedelayser = hbdelayser.clone(subdet=cms.untracked.string("HE"))
+
+hodelayser = hbdelayser.clone(subdet=cms.untracked.string("HO"))
+hodelayser.eventDataPset.hbheRechitLabel = cms.untracked.InputTag("") # blank this label for HO,HF
+hfdelayser = hodelayser.clone(subdet=cms.untracked.string("HF"))
+
+hodelayser.eventDataPset.hoRechitLabel=cms.untracked.InputTag("horeco")
+#hodelayser.eventDataPset.hoDigiLabel=cms.untracked.InputTag("hcalDigis")
+
+hfdelayser.eventDataPset.hfRechitLabel=cms.untracked.InputTag("hfreco")
+hfdelayser.eventDataPset.hfDigiLabel=cms.untracked.InputTag("hcalDigis")
