4a4a433ceaf00815dbf2456a2865c390135f08f7
dudero <>
2010-02-27 00:42:35 +0000

    add book2dvarx and book2dvary

--- MyEDmodules/MyAnalUtilities/interface/myAnalHistos.hh
+++ MyEDmodules/MyAnalUtilities/interface/myAnalHistos.hh
@@ -16,7 +16,7 @@
 //
 // Original Author:  Phillip Russell DUDERO
 //         Created:  Tue Sep  9 13:11:09 CEST 2008
-// $Id: myAnalHistos.hh,v 1.6 2009/10/27 02:45:39 dudero Exp $
+// $Id: myAnalHistos.hh,v 1.7 2009/11/09 01:10:56 dudero Exp $
 //
 //
 
@@ -45,7 +45,13 @@
 class myAnalHistos {
 public:
   // set nbinsy to 0 for 1-D
-  typedef struct {
+  struct HistoParams_t {
+    HistoParams_t() {}
+    HistoParams_t(const std::string& inname, const std::string& intit,
+		  uint32_t innbinsx,double inminx, double inmaxx,
+		  uint32_t innbinsy=0,double inminy=0.0, double inmaxy=0.0) :
+      name(inname), title(intit), nbinsx(innbinsx), minx(inminx), maxx(inmaxx),
+      nbinsy(innbinsy), miny(inminy), maxy(inmaxy) {}
     std::string name;
     std::string title;
     uint32_t    nbinsx;
@@ -54,8 +60,7 @@ public:
     uint32_t    nbinsy;
     double      miny;
     double      maxy;
-  }
-  HistoParams_t;
+  };
 
   explicit myAnalHistos(const std::string& dirdescr);
   explicit myAnalHistos(const std::string& dirdescr,
@@ -66,6 +71,14 @@ public:
   template<class T> void book2d(const HistoParams_t& pars);
   template<class T> void book1d(const std::vector<HistoParams_t>& v_pars);
   template<class T> void book2d(const std::vector<HistoParams_t>& v_pars);
+  template<class T> void book2dvarx(const std::string& name,
+				    const char *title,
+				    int nbinsx, const double xbins[],
+				    int nbinsy, double ymin, double ymax);
+  template<class T> void book2dvary(const std::string& name,
+				    const char *title,
+				    int nbinsx, double xmin, double xmax,
+				    int nbinsy, const double ybins[]);
   template<class T> T *get(const std::string& hname);
 
   template<class T> void fill1d(const std::map<std::string,double>& vals);
@@ -122,10 +135,12 @@ void myAnalHistos::book1d(const HistoParams_t& pars)
 
   //edm::LogInfo("booking histogram ") << histo.pars.name << std::endl;
   std::cout << "booking histogram " << histo.pars.name << std::endl;
+  //std::cout << ", this=" << (int)this << std::endl;
   histo.ptr = dir_->make <T> (histo.pars.name.c_str(),
 			      histo.pars.title.c_str(),
 			      histo.pars.nbinsx, histo.pars.minx, histo.pars.maxx);
   hm_histos_[histo.pars.name.c_str()] = histo;
+  //std::cout << "hm_histos_.size() = " << hm_histos_.size() << std::endl;
 }
 
 //======================================================================
@@ -148,7 +163,8 @@ void myAnalHistos::book2d(const HistoParams_t& pars)
   Histo_t histo;
   histo.pars = pars;
 
-  edm::LogInfo("booking histogram ") << histo.pars.name << std::endl;
+  //edm::LogInfo("booking histogram ") << histo.pars.name << std::endl;
+  std::cout << "booking histogram " << histo.pars.name << std::endl;
   histo.ptr = dir_->make <T> (histo.pars.name.c_str(), histo.pars.title.c_str(),
 			      histo.pars.nbinsx, histo.pars.minx, histo.pars.maxx,
 			      histo.pars.nbinsy, histo.pars.miny, histo.pars.maxy);
@@ -167,16 +183,67 @@ void myAnalHistos::book2d(const std::vector<HistoParams_t>& v_pars)
 //======================================================================
 
 template<class T>
+void myAnalHistos::book2dvarx(const std::string& name,
+			      const char *title,
+			      int nbinsx, const double xbins[],
+			      int nbinsy, double ymin, double ymax)
+{
+  Histo_t histo;
+  histo.pars.name = name;
+  histo.pars.title=std::string(title);
+  histo.pars.nbinsx=nbinsx;
+  histo.pars.nbinsy=nbinsy;
+  histo.pars.miny=ymin;
+  histo.pars.maxy=ymax;
+
+  //edm::LogInfo("booking histogram ") << histo.pars.name << std::endl;
+  std::cout << "booking histogram " << name << std::endl;
+  histo.ptr = dir_->make <T> (name.c_str(), title,
+			      nbinsx, xbins,
+			      nbinsy, ymin, ymax);
+  hm_histos_[name.c_str()] = histo;
+}
+
+//======================================================================
+
+template<class T>
+void myAnalHistos::book2dvary(const std::string& name,
+			      const char *title,
+			      int nbinsx, double xmin, double xmax,
+			      int nbinsy, const double ybins[])
+{
+  Histo_t histo;
+  histo.pars.name=name;
+  histo.pars.title=std::string(title);
+  histo.pars.nbinsx=nbinsx;
+  histo.pars.minx=xmin;
+  histo.pars.maxx=xmax;
+  histo.pars.nbinsy=nbinsy;
+
+  //edm::LogInfo("booking histogram ") << histo.pars.name << std::endl;
+  std::cout << "booking histogram " << name << std::endl;
+  histo.ptr = dir_->make <T> (name.c_str(), title,
+			      nbinsx, xmin, xmax,
+			      nbinsy, ybins);
+  hm_histos_[name.c_str()] = histo;
+}
+
+//======================================================================
+
+template<class T>
 void
 myAnalHistos::bookClone(const std::string& cloneName,const T& h)
 {
-  edm::LogInfo("booking clone histogram ") << cloneName << std::endl;
+  //edm::LogInfo("booking clone histogram ") << cloneName << std::endl;
+  std::cout << "booking clone histogram " << cloneName;
+  std::cout << ", this=" << (int)this << std::endl;
   Histo_t histo;
   histo.ptr = (TH1 *)dir_->make <T, T> (h);
-  std::cout << "Before SetName: " << histo.ptr->GetName() << std::endl;
+  //std::cout << "Before SetName: " << histo.ptr->GetName() << std::endl;
   histo.ptr->SetName(cloneName.c_str());
-  std::cout << "After  SetName: " << histo.ptr->GetName() << std::endl;
+  //std::cout << "After  SetName: " << histo.ptr->GetName() << std::endl;
   hm_histos_[cloneName.c_str()] = histo;
+  //std::cout << "hm_histos_.size() = " << hm_histos_.size() << std::endl;
 }
 
 //======================================================================
@@ -186,13 +253,16 @@ void
 myAnalHistos::fill1d(const std::string& hname,double val,double weight)
 {
   myHashmap_t::const_iterator ith;
-
   ith = hm_histos_.find(hname.c_str());
+
   if (ith != hm_histos_.end()) {
     T *p = (T *)ith->second.ptr;
+    if (!p) std::cout << "NULL POINTER for histo " << hname << "!!!" << std::endl;
+
     p->Fill(val,weight);
   } else {
-    edm::LogError("Couldn't find hash for " + hname + "! ") 
+    //    edm::LogError("Couldn't find hash for " + hname + "! ") 
+     throw cms::Exception("Couldn't find hash for " + hname + "! ") 
       << val << "\t" << weight << std::endl;
   }
 }
@@ -228,6 +298,7 @@ myAnalHistos::fill2d(const std::string& hname,double valx,double valy,double wei
   ith = hm_histos_.find(hname.c_str());
   if (ith != hm_histos_.end()) {
     T *p = (T *)ith->second.ptr;
+    if (!p) std::cout << "NULL POINTER for histo " << hname << "!!!" << std::endl;
     p->Fill(valx,valy,weight);
   } else {
     edm::LogError("Couldn't find hash for " + hname + "!")
