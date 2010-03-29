92ff195d3776a5137930b26c34136382c3275a78
dudero <>
2010-03-29 13:27:22 +0000

    first working version

--- MyEDmodules/HcalDelayTuner/test/btacronmaster.sh
+++ MyEDmodules/HcalDelayTuner/test/btacronmaster.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
 TZFILE=~/public/inbox/btajc.tz
-JCFILE=btajc.rc
-CMSREL=CMSSW_3_3_6
 MYDATE=`date +%Y%m%d_%H%M%S`
-#
-WRKDIR=~/scratch0/data/batchdir
-PROJDIR=~/scratch0/${CMSREL}/src/MyEDmodules/HcalDelayTuner/
-PYDIR=${PROJDIR}/python
 
+#======================================================================
+# First check existence of running jobs; if done,
+# Clean up and put results in outbox
+#
+BATCHDIR=~/scratch0/data/batchdir
+cd ${BATCHDIR}
+NRUNNINGJOBS=`ls | grep -c btaoutput`
+if (( ${NRUNNINGJOBS} ))
+then
+    for dir in `ls -d btaoutput*`
+    do
+      NJOBSNOTFINISHED=`ls -a $dir | grep -c "\.btacronmaster_activejob"`
+      if (( ${NJOBSNOTFINISHED} == 0 ))
+      then
+	  echo "$MYDATE: Cleaning up.. " ${dir}
+	  tar czvf ${dir}.tz ${dir}/* >/dev/null
+	  mv ${dir}.tz ~/public/outbox
+	  rm -rf ${dir}
+	  echo "$MYDATE: Done with " ${dir}
+      fi
+    done
+fi
+
+#Now check existence of new jobs to run
 if [[ ! -e ${TZFILE} ]] 
 then
-    echo "nothing to see here, move along."
     exit
 fi
+
+JCFILE=btajc.rc
+CMSREL=CMSSW_3_3_6
+#
+WRKDIR=${BATCHDIR}/btaoutput_${MYDATE}
+PROJDIR=~/scratch0/${CMSREL}/src/MyEDmodules/HcalDelayTuner/
+PYDIR=${PROJDIR}/python
+mkdir ${WRKDIR}
+cd ${WRKDIR}
+tar xzvf ${TZFILE} >/dev/null
+rm ${TZFILE}
+
+echo "============================================"
+echo "$MYDATE: found job control file, processing."
+
 #
 #======================================================================
 # Process the incoming job specification tarball
@@ -24,7 +56,7 @@ rm ${TZFILE}
 
 if [[ ! -e ${JCFILE} ]] 
 then
-    echo "${JCFILE} absent from ${TZFILE}, aborting."
+    echo "$MYDATE: ${JCFILE} absent from ${TZFILE}, aborting."
     exit
 fi
 
@@ -37,7 +69,7 @@ rm ${JCFILE}
 #
 if (( ${#EMAILLOG2} > 0 ))
 then
-    echo "Mailing to " ${EMAILLOG2}
+    echo "$MYDATE: Mailing to " ${EMAILLOG2}
 fi
 #
 #======================================================================
@@ -47,16 +79,19 @@ if (( ${#DATASET} > 0 ))
 then
     for run in ${RUNS}
     do
-      echo "Making cfi for run #${run}"
+      echo "$MYDATE: Making cfi for run #${run}"
       ~/private/bin/makelistpy4castor.sh ${DATASET} ${run} >${PYDIR}/run${run}files_cfi.py
       if (( `grep -c root ${PYDIR}/run${run}files_cfi.py` == 0 ))
       then
-	  echo "No files found for run ${run} in dataset ${DATASET}! " 
+	  echo "$MYDATE: No files found for run ${run} in dataset ${DATASET}! " 
 	  exit
       fi
     done
 fi
-
+#
+#======================================================================
+# set up CMSSW environment
+#
 source /afs/cern.ch/cms/sw/cmsset_default.sh
 cd ${PROJDIR}
 #cmsenv
@@ -72,16 +107,16 @@ then
     for run in ${RUNS}
     do
 #     PREFIX=myskim_run${run}_pool
-      echo "Running skim file ${SKIMFILE}"
+      echo "$MYDATE: Running skim file ${SKIMFILE}"
       cp temp.rc skim_setup_${run}.rc
       echo "RUN=${run}" >>skim_setup_${run}.rc
       ${PROJDIR}/test/runSkimBatch.sh skim_setup_${run}.rc
 #     if [[ -e ${WRKDIR}/${SKIMOUTPUT} ]]
 #     then
-#	  echo "Moving skim output to castor..."
+#	  echo "$MYDATE: Moving skim output to castor..."
 #	  rfcp ${WRKDIR}/${SKIMOUTPUT} ${CASTORSKIMLOC}
 #	  rm $WRKDIR/${SKIMOUTPUT}
-#	  echo "done."
+#	  echo "$MYDATE: done."
 #	  makelistpy4afs.sh "rfio:${CASTORSKIMLOC}/${SKIMOUTPUT}" >${PYDIR}/${PREFIX}_cfi.py
 #     fi
     done
@@ -94,9 +129,21 @@ fi
 #
 if (( ${#ANALFILE} > 0 ))
 then
-    echo "Running anal file" ${ANALFILE}
-    mv ${ANALFILE} anal_setup.rc
-    ${PROJDIR}/test/runBeamAnalBatch.sh
+    cp ${WRKDIR}/${ANALFILE} temp.rc
+    for run in ${RUNS}
+    do
+#     PREFIX=myskim_run${run}_pool
+      echo "$MYDATE: Running anal file " ${ANALFILE} " on run ${run}"
+      cp temp.rc anal_setup.rc
+      echo "RUN=${run}" >>anal_setup.rc
+      echo "RUNMODE=BATCH" >>anal_setup.rc
+      BATCH_SEMA4=${WRKDIR}/.btacronmaster_activejob_${run}
+      touch ${BATCH_SEMA4}
+      echo "BATCH_SEMA4=${BATCH_SEMA4}" >>anal_setup.rc
+      ${PROJDIR}/test/runBeamAnalBatch.sh
+    done
+    rm temp.rc
+    exit
 fi
 #
 #======================================================================
@@ -104,11 +151,5 @@ fi
 #
 if (( ${#PLOTFILE} > 0 ))
 then
-    echo "Plotting " ${PLOTFILE}
+    echo "$MYDATE: Plotting " ${PLOTFILE}
 fi
-#
-#======================================================================
-# Clean up and put results in outbox
-#
-tar czvf btaoutput_${MYDATE}.tz  *.*
-mv btaoutput_${MYDATE}.tz ~/public/outbox
