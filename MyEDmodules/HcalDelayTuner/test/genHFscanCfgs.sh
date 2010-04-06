9cfe981cf16d7d471e9224de587375a9d3dd61bf
dudero <>
2010-04-06 09:50:46 +0000

    add setup for points 5,4,2

--- MyEDmodules/HcalDelayTuner/test/genHFscanCfgs.sh
+++ MyEDmodules/HcalDelayTuner/test/genHFscanCfgs.sh
@@ -58,58 +58,80 @@ BASENAMEFMT="bta_run%s_hfscan_delay=%d"
 
 ########################################
 # RUN 132477
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 12`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:6-132477:33\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 10`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:36-132477:62\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 11`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:65-132477:90\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 8`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:94-132477:119\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 6`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:122-132477:147\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 9`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:150-132477:175\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 13`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:180-132477:224\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+
+# cp anal_setup_hfscan.rc anal_setup.rc
+# BASENAME=`printf "$BASENAMEFMT" 132477 7`
+# echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
+# echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
+# echo "LSRANGES=\'132477:237-132477:MAX\'" >> anal_setup.rc
+# ./genbtapy.sh 132477
+########################################
+# RUN 132658
 cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 12`
+BASENAME=`printf "$BASENAMEFMT" 132658 5`
 echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
 echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:6-132477:33\'" >> anal_setup.rc
-./genbtapy.sh 132477
+echo "LSRANGES=\'132658:1-132658:\'" >> anal_setup.rc
+./genbtapy.sh 132658
 
 cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 10`
+BASENAME=`printf "$BASENAMEFMT" 132658 4`
 echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
 echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:36-132477:62\'" >> anal_setup.rc
-./genbtapy.sh 132477
+echo "LSRANGES=\'132658:-132658:\'" >> anal_setup.rc
+./genbtapy.sh 132658
 
 cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 11`
+BASENAME=`printf "$BASENAMEFMT" 132658 2`
 echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
 echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:65-132477:90\'" >> anal_setup.rc
-./genbtapy.sh 132477
-
-cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 8`
-echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
-echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:94-132477:119\'" >> anal_setup.rc
-./genbtapy.sh 132477
-
-cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 6`
-echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
-echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:122-132477:147\'" >> anal_setup.rc
-./genbtapy.sh 132477
-
-cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 9`
-echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
-echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:150-132477:175\'" >> anal_setup.rc
-./genbtapy.sh 132477
-
-cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 13`
-echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
-echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:180-132477:224\'" >> anal_setup.rc
-./genbtapy.sh 132477
-
-cp anal_setup_hfscan.rc anal_setup.rc
-BASENAME=`printf "$BASENAMEFMT" 132477 7`
-echo "ANALOUTPUT=$BASENAME.root" >> anal_setup.rc
-echo "CFGFILE=$BASENAME.py" >> anal_setup.rc
-echo "LSRANGES=\'132477:237-132477:MAX\'" >> anal_setup.rc
-./genbtapy.sh 132477
+echo "LSRANGES=\'132658:-132658:\'" >> anal_setup.rc
+./genbtapy.sh 132658
