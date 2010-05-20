f70b5d6ede4a5f3f941012a6f98c604eea87f4eb
dudero <>
2010-05-20 15:27:31 +0000

    add hbheFlagBit parameter

--- MyEDmodules/METcleaningComparator/test/metcleaningcomparator_cfg.py
+++ MyEDmodules/METcleaningComparator/test/metcleaningcomparator_cfg.py
@@ -3,13 +3,14 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("METCLEAN")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.TFileService = cms.Service("TFileService", 
     fileName = cms.string("metcleaningcomparison.root"),
     closeFileFast = cms.untracked.bool(False)
 )
 
-#process.maxEvents = cms.untracked.PSet( output = cms.untracked.int32(10000) )
+#process.maxEvents = cms.untracked.PSet( output = cms.untracked.int32(100) )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #######################################################################
@@ -45,7 +46,6 @@ process.trigpath = cms.Path(process.jetnobptx*(~process.bscorbptx))
 
 # Standard configurations
 process.load('Configuration/StandardSequences/Services_cff')
-process.load('FWCore/MessageService/MessageLogger_cfi')
 process.load('Configuration/StandardSequences/GeometryExtended_cff')
 process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
 process.load('Configuration/StandardSequences/Reconstruction_cff')
@@ -83,7 +83,7 @@ process.rereco_step = cms.Path(process.caloTowersRec*(process.recoJets*process.r
 
 process.load("TCMETcleaned357.CleanedTCMETProducer.cleanedtcmetproducer_cfi")
 process.tcMetClean.tcmetInputTag     = cms.InputTag("met","","RECO")  # recoCaloMETs_met__RECO
-process.tcMetClean.useHFcorrection   = cms.bool(True)
+process.tcMetClean.useHFcorrection   = cms.bool(False)
 process.tcMetClean.useECALcorrection = cms.bool(False)
 process.tcMetClean.useHCALcorrection = cms.bool(True)
 process.tcMetClean.alias             = cms.string("TCcleanedCaloMET")
@@ -134,20 +134,26 @@ process.output.outputCommands = cms.untracked.vstring("drop *",
 process.compare = cms.EDAnalyzer('METcleaningComparator',
                                  dirtyInput = cms.untracked.PSet(
     hbheRechitLabel = cms.untracked.InputTag("hbhereco"),
-    caloMETlabel    = cms.untracked.InputTag("met","","RECO")
+    caloMETlabel    = cms.untracked.InputTag("met","","RECO"),
+#    verbose         = cms.untracked.bool(True)
     ),
                                  tcmetCleanOutput = cms.untracked.PSet(
-    recoMETlabel    = cms.untracked.InputTag("tcMetClean","","METCLEAN")
+    recoMETlabel    = cms.untracked.InputTag("tcMetClean","","METCLEAN"),
+#    verbose         = cms.untracked.bool(True)
     ),
                                  reflagCleanOutput = cms.untracked.PSet(
     hbheRechitLabel = cms.untracked.InputTag("hbherecoReflagged"),
-    caloMETlabel    = cms.untracked.InputTag("met","","METCLEAN")
+    caloMETlabel    = cms.untracked.InputTag("met","","METCLEAN"),
+#    verbose         = cms.untracked.bool(True)
     ),
                                  evfiltCleanOutput = cms.untracked.PSet(
     trgResultsLabel = cms.untracked.InputTag("TriggerResults","","METCLEAN"),
-    caloMETlabel    = cms.untracked.InputTag("met","","METCLEAN")
+    caloMETlabel    = cms.untracked.InputTag("met","","METCLEAN"),
+#    verbose         = cms.untracked.bool(True)
     ),
-                                 evfiltPathName = cms.untracked.string("evfiltpath")
+                                 evfiltPathName = cms.untracked.string("evfiltpath"),
+                                 hbheFlagBit    = cms.untracked.int32(31)
+# bit 31 is UserDefinedBit0; this duplicates the setting inside hbherechitreflaggerJETMET_cfi.py
 )
 
 #######################################################################
