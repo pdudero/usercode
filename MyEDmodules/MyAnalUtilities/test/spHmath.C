85dcfa46196473bc66b6b357660359f50fd614f0
dudero <>
2012-05-14 23:26:59 +0000

    work on integright, add integrightup, integsigned, catmultihist2

--- MyEDmodules/MyAnalUtilities/test/spHmath.C
+++ MyEDmodules/MyAnalUtilities/test/spHmath.C
@@ -50,6 +50,7 @@ bool parseBinSpec( const string& binspec,
 }                                                        // parseBinSpec
 
 //======================================================================
+// Sweep right and integrate to the left of the sweep line
 
 TH1 *IntegrateLeft(TH1 *h)
 {
@@ -70,20 +71,36 @@ TH1 *IntegrateLeft(TH1 *h)
 }                                                       // IntegrateLeft
 
 //======================================================================
+// Sweep right and integrate to the right of the sweep line
 
-TH1 *IntegrateRight(TH1 *h,double skipbinatx=-9e99)
+TH1 *IntegrateRight(TH1 *h,
+		    const string& xrangestr,
+		    double skipbinatx=-9e99)
 {
   string newname = string(h->GetName())+"_integright";
   TH1 *hcum = (TH1 *)h->Clone(newname.c_str());
   hcum->Reset();
 
-  int nbins = hcum->GetNbinsX();
-  double htotal = h->Integral(1,nbins+1);
-  int   skipbin = nbins+2;
+  int lobin = 0;
+  int hibin = hcum->GetNbinsX()+1; // includes overflow
+  if (xrangestr.size()) {
+    vector<string> v_tokens;
+    Tokenize(xrangestr,v_tokens,"-");
+    if (v_tokens.size() != 2) {
+      cerr << "Malformed xrange spec string " << xrangestr << endl;
+      return NULL;
+    }
+    float xmin = str2flt(v_tokens[0]);
+    float xmax = str2flt(v_tokens[1]);
+    lobin = h->FindBin(xmin);
+    hibin = h->FindBin(xmax); // hope that kCanRebin isn't set
+  }
+  double htotal = h->Integral(lobin,hibin);
+  int   skipbin = hcum->GetNbinsX()+2;
   float skipcontent = 0.0;
 
   if (skipbinatx > -9e99) {
-    for (int i=1; i<=nbins+1 ; i++) { // includes overflow
+    for (int i=lobin; i<=hibin ; i++) {
       if ((h->GetXaxis()->GetBinLowEdge(i) < skipbinatx) &&
 	  (h->GetXaxis()->GetBinUpEdge(i)  > skipbinatx)   ) {
 	skipbin      = i;
@@ -94,8 +111,8 @@ TH1 *IntegrateRight(TH1 *h,double skipbinatx=-9e99)
   }
 
   // Include the overflow bin
-  for (int i=1; i<=nbins+1 ; i++) { // includes overflow
-    double integral = h->Integral(i,nbins+1);
+  for (int i=lobin; i<=hibin; i++) {
+    double integral = h->Integral(i,hibin);
     if (i > skipbin) skipcontent=0.0;
     hcum->SetBinContent(i,(integral-skipcontent)/htotal);
     //hcum->SetBinError(i,sqrt(integral-skipcontent)/htotal);
@@ -104,6 +121,111 @@ TH1 *IntegrateRight(TH1 *h,double skipbinatx=-9e99)
   return hcum;
 }                                                      // IntegrateRight
 
+
+//======================================================================
+// 1D integral: Sweep out from zero on both sides, integ inside the sweep lines
+
+TH1 *IntegrateSigned(TH1 *h)
+{
+  int nbins = h->GetNbinsX();
+
+  int bin0  = h->FindFixBin(0.);
+
+  if (bin0 == 0 || bin0==nbins+1) {
+    cerr << "Histogram " << h->GetName() << " does not have 0 in the x-range" << endl;
+    return 0;
+  }
+  TAxis *xax = h->GetXaxis();
+  cout << "lowedge(bin0) = " << xax->GetBinLowEdge(bin0);
+  cout << ", upedge(bin0) = " << xax->GetBinUpEdge(bin0) << endl;
+
+  if (xax->GetBinLowEdge(bin0)!=0) {
+    cerr << "Histogram " << h->GetName() << " does not have 0 as a bin lo edge" << endl;
+    return 0;
+  }
+
+  int newnbins;
+  double xmax;
+
+  if (nbins-bin0+1 > bin0-1) {
+    newnbins = bin0-1;
+    xmax     = fabs(xax->GetBinLowEdge(1));
+  } else {
+    newnbins = nbins-bin0+1;
+    xmax     = xax->GetBinUpEdge(nbins);
+  }
+
+  string newname = string(h->GetName())+"_integsigned";
+  cout<<"Booking new TH1D(\""<<newname<<"\",\""<<newname<<"\","<<newnbins<<",0,"<<xmax<<")"<<endl;
+  TH1 *hcum = new TH1D(newname.c_str(), newname.c_str(), newnbins,0,xmax);
+  hcum->Reset();
+
+  double htotal = h->Integral(1,nbins+1);
+
+  // Include the overflow bin
+  for (int i=1; i<=newnbins+1 ; i++) { // includes overflow
+    //cout<<i<<": integrating bins " << bin0-i << "-" << bin0+i-1;
+    //cout<<", "<<xax->GetBinLowEdge(bin0-i)<<"to"<<xax->GetBinUpEdge(bin0+i-1)<< endl;
+    double integral = h->Integral(bin0-i,bin0+i-1);
+    hcum->SetBinContent(i,integral/htotal);
+  }
+
+  return hcum;
+}                                                     // IntegrateSigned
+
+//======================================================================
+// 2D integral: Sweep right/up and integrate to the right/up of the sweep lines
+
+TH2 *IntegrateRightAndUp(TH2 *h,
+			 const string& xrangestr,
+			 const string& yrangestr)
+{
+  string newname = string(h->GetName())+"_integrightup";
+  TH2 *hcum = (TH2 *)h->Clone(newname.c_str());
+  hcum->Reset();
+
+  int lobinx = 0;
+  int hibinx = hcum->GetNbinsX()+1; // includes overflow
+  int lobiny = 0;
+  int hibiny = hcum->GetNbinsY()+1; // includes overflow
+  if (xrangestr.size()) {
+    vector<string> v_tokens;
+    Tokenize(xrangestr,v_tokens,"-");
+    if (v_tokens.size() != 2) {
+      cerr << "Malformed xrange spec string " << xrangestr << endl;
+      return NULL;
+    }
+    float xmin = str2flt(v_tokens[0]);
+    float xmax = str2flt(v_tokens[1]);
+    lobinx = h->GetXaxis()->FindBin(xmin);
+    hibinx = h->GetXaxis()->FindBin(xmax); // hope that kCanRebin isn't set
+  }
+  if (yrangestr.size()) {
+    vector<string> v_tokens;
+    Tokenize(yrangestr,v_tokens,"-");
+    if (v_tokens.size() != 2) {
+      cerr << "Malformed yrange spec string " << yrangestr << endl;
+      return NULL;
+    }
+    float ymin = str2flt(v_tokens[0]);
+    float ymax = str2flt(v_tokens[1]);
+    lobiny = h->GetYaxis()->FindBin(ymin);
+    hibiny = h->GetYaxis()->FindBin(ymax); // hope that kCanRebin isn't set
+  }
+
+  double htotal = h->Integral(lobinx,hibinx,lobiny,hibiny);
+
+  // Include the overflow bin
+  for (int ix=lobinx; ix<=hibinx; ix++) {
+    for (int iy=lobiny; iy<=hibiny; iy++) {
+      double integral = h->Integral(ix,hibinx,iy,hibiny);
+      hcum->SetBinContent(ix,iy,(integral)/htotal);
+      //hcum->SetBinError(i,sqrt(integral)/htotal);
+    }
+  }
+
+  return hcum;
+}                                                 // IntegrateRightAndUp
 
 //======================================================================
 
@@ -254,9 +376,10 @@ void sliceHistos(const string slicetype,
 }                                                         // sliceHistos
 
 //======================================================================
-
-wTH1 *catMultiHist(const string& myhid,
-		   const string& multihid)
+// good for concatenating run histos, each bin is labeled with a run number
+//
+wTH1 *catMultiHist1(const string& myhid,
+		    const string& multihid)
 {
   wTH1D *rethist(0);
   map<string,TH1 *> m_histos2cat;
@@ -296,6 +419,62 @@ wTH1 *catMultiHist(const string& myhid,
 
   return (wTH1 *)rethist;
 }                                                        // catMultiHist
+
+//======================================================================
+// This one preserves the x-axis of the input histograms, heterogeneously
+// binned histograms may be concatenated together, provided the endpoints
+// line up...
+//
+
+wTH1 *catMultiHist2(const string& myhid,
+		    const string& multihid)
+{
+  wTH1D *rethist(0);
+  map<string,TH1 *> m_histos2cat;
+  vector<double> binloedges;
+
+  double xmax=-9e99;
+  for (int i=0; ; i++) {
+    string histid = multihid+"_"+int2str(i);
+    TH1 *h1 = findHisto(histid, "hit the end of histo multiset");
+    if (h1) {
+      for (int ibin=1; ibin<=h1->GetNbinsX(); ibin++) {
+	binloedges.push_back(h1->GetBinLowEdge(ibin));
+      }
+      if (h1->GetXaxis()->GetXmax() > xmax) xmax = h1->GetXaxis()->GetXmax();
+
+      m_histos2cat.insert(pair<string,TH1 *>(string(h1->GetName()),h1));
+    }
+    else
+      break;
+  }
+
+  int totalbins=binloedges.size();
+
+  if (!totalbins) return NULL;
+  sort(binloedges.begin(),binloedges.end());
+
+  binloedges.push_back(xmax);
+
+  if (m_histos2cat.size()) {
+    rethist    = new wTH1D(new TH1D(myhid.c_str(),myhid.c_str(),totalbins,&binloedges[0]));
+    TH1  *hout = rethist->histo();
+    hout->UseCurrentStyle();
+    int glbin=1;
+    map<string,TH1 *>::const_iterator it;
+    for (it=m_histos2cat.begin();it != m_histos2cat.end(); it++) {
+      TH1 *hin  = it->second;
+      int nbins = hin->GetNbinsX();
+      string hname = it->first;
+      for (int ibin=1; ibin<=nbins; ibin++) {
+	glbin = hout->FindFixBin(hin->GetBinCenter(ibin));
+	hout->SetBinContent(glbin,hin->GetBinContent(ibin));
+      }
+    }
+  }
+
+  return (wTH1 *)rethist;
+}                                                       // catMultiHist2
 
 //======================================================================
 
@@ -656,7 +835,7 @@ processHmathSection(FILE *fp,
 
       TH1 *tmph1 = findHisto(v_tokens[0]); if (!tmph1) continue;
       TH1 *tmph2 = findHisto(v_tokens[1]); if (!tmph2) continue;
-      TH1 *hres = (TH1 *)tmph1->Clone();
+      TH1 *hres = (TH1 *)tmph1->Clone(hid->c_str());
       //hres->Reset();
 
       if      (theline.find('-') != string::npos) hres->Add(tmph2,-1.0);
@@ -681,7 +860,7 @@ processHmathSection(FILE *fp,
       }
       TH1 *firstone = (TH1 *)findHisto(v_tokens[0]);
       if (!firstone) exit(-1);
-      h1 = (TH1 *)firstone->Clone();
+      h1 = (TH1 *)firstone->Clone(hid->c_str());
       if (key == "weightsum")
 	h1->SetBit(TH1::kIsAverage);  // <========== Addends also have to have this set.
       for (unsigned i=1; i<v_tokens.size(); i++) {
@@ -702,9 +881,17 @@ processHmathSection(FILE *fp,
       if (h1) {
 	cerr << "histo already defined" << endl; continue;
       }
-      TH1 *tmph1 = findHisto(value,"histo operand must be defined before math ops");
-      if (!tmph1) exit(-1);
-      h1 = (TH1 *)IntegrateRight(tmph1,skipbinatx);
+      Tokenize(value,v_tokens,":");
+      if (v_tokens.size()==1) {
+	TH1 *tmph1 = findHisto(value,"histo operand must be defined before math ops");
+	if (!tmph1) exit(-1);
+	h1 = (TH1 *)IntegrateRight(tmph1,"",skipbinatx);
+      } else if (v_tokens.size()==2) {
+	string xrangestr = v_tokens[1];
+	TH1 *tmph1 = findHisto(v_tokens[0],"histo operand must be defined before math ops");
+	if (!tmph1) exit(-1);
+	h1 = (TH1 *)IntegrateRight(tmph1,xrangestr,skipbinatx);
+      }
       wTH1 *wh = new wTH1(h1);
       v_histos.push_back(pair<string,wTH1 *>(*hid,wh));
       glmap_id2histo.insert(pair<string,wTH1 *>(*hid,wh));
@@ -725,11 +912,52 @@ processHmathSection(FILE *fp,
       v_histos.push_back(pair<string,wTH1 *>(*hid,wh));
       glmap_id2histo.insert(pair<string,wTH1 *>(*hid,wh));
 
-    } else if (key == "skipbinatx")
+    //------------------------------
+    } else if (key == "integsigned") {  // for histos symmetric about zero
+    //------------------------------
+      if (!hid) {
+	cerr << "id key must be defined first in the section" << endl; continue;
+      }
+      if (h1) {
+	cerr << "histo already defined" << endl; continue;
+      }
+
+      TH1 *tmph1 = findHisto(value,"histo operand must be defined before math ops");
+      if (!tmph1) exit(-1);
+      h1 = (TH1 *)IntegrateSigned(tmph1);
+      wTH1 *wh = new wTH1(h1);
+      v_histos.push_back(pair<string,wTH1 *>(*hid,wh));
+      glmap_id2histo.insert(pair<string,wTH1 *>(*hid,wh));
+
+    //------------------------------
+    } else if (key == "integrightup") {  // for 2d histos
+    //------------------------------
+      if (!hid) {
+	cerr << "id key must be defined first in the section" << endl; continue;
+      }
+      if (h1) {
+	cerr << "histo already defined" << endl; continue;
+      }
+      Tokenize(value,v_tokens,":");
+      if (v_tokens.size()==1) {
+	TH2 *tmph2 = (TH2 *)findHisto(value,"histo operand must be defined before math ops");
+	if (!tmph2) exit(-1);
+	if (!tmph2->InheritsFrom("TH2")) {
+	  cerr << "integrightup: the histogram to integrate is not 2d" << endl; continue;
+	}
+	h1 = (TH1 *)IntegrateRightAndUp(tmph2,"","");
+      }
+      wTH1 *wh = new wTH1(h1);
+      v_histos.push_back(pair<string,wTH1 *>(*hid,wh));
+      glmap_id2histo.insert(pair<string,wTH1 *>(*hid,wh));
+
+    //------------------------------
+    } else if (key == "skipbinatx") {
+    //------------------------------
       skipbinatx = str2flt(value);
 
     //------------------------------
-    else if (key == "projectx") {
+    } else if (key == "projectx") {
     //------------------------------
       if (!hid) {
 	cerr << "id key must be defined first in the section" << endl; continue;
@@ -811,7 +1039,7 @@ processHmathSection(FILE *fp,
 	cerr << "histo already defined" << endl; continue;
       }
 
-      wTH1 *wh = catMultiHist(*hid,value);
+      wTH1 *wh = catMultiHist2(*hid,value);
       v_histos.push_back(pair<string,wTH1 *>(*hid,wh));
       glmap_id2histo.insert(pair<string,wTH1 *>(*hid,wh));
       
