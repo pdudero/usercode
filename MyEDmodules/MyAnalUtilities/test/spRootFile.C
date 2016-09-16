7ae30650e55a6f516721ad7679d5601ebe1dacaa
Phillip <pdudero@gmail.com>
2016-09-16 16:59:25 -0500

    add closeFiles,save2File,saveAs

--- MyEDmodules/MyAnalUtilities/test/spRootFile.C
+++ MyEDmodules/MyAnalUtilities/test/spRootFile.C
@@ -1,6 +1,9 @@
+#include "TObjString.h"
 #include "TDCacheFile.h"
 
 static map<string, TFile *> glmap_id2rootfile;
+
+//======================================================================
 
 TFile *openRootFile(const std::string& rootfn, const std::string& option="")
 {
@@ -11,7 +14,10 @@ TFile *openRootFile(const std::string& rootfn, const std::string& option="")
   if( it != glmap_id2rootfile.end() ) {
     rootfile = it->second;
     if (!rootfile->IsOpen())
-      rootfile->Open(rootfn.c_str(),option.c_str());
+      if (rootfn.find(":"))
+	rootfile = TFile::Open(rootfn.c_str(),option.c_str());
+      else
+	rootfile->Open(rootfn.c_str(),option.c_str());
     else
       rootfile->cd();
   }  else {
@@ -19,7 +25,9 @@ TFile *openRootFile(const std::string& rootfn, const std::string& option="")
     if (strstr(rootfn.c_str(),"dcache") ||
 	strstr(rootfn.c_str(),"dcap")      ) {
       rootfile = new TDCacheFile(rootfn.c_str(),option.c_str());
-    } else
+    } else if (rootfn.find(":"))
+      rootfile = TFile::Open(rootfn.c_str(),option.c_str());
+    else
       rootfile = new TFile(rootfn.c_str(),option.c_str());
   
     if( rootfile->IsZombie() ) {
@@ -30,4 +38,99 @@ TFile *openRootFile(const std::string& rootfn, const std::string& option="")
     }
   }
   return rootfile;
+}                                                        // openRootFile
+
+//======================================================================
+
+void closeFiles(void)
+{
+  TFile *rootfile = NULL;
+
+  // Now check to see if this file has already been opened...
+  map<string,TFile*>::const_iterator it;
+  for( it = glmap_id2rootfile.begin(); it != glmap_id2rootfile.end(); it++ ) {
+    rootfile = it->second;
+    if (rootfile)
+      rootfile->Close();
+  }
+
+}
+
+//======================================================================
+
+void save2File(TNamed *obj, string outspec)
+{
+  TFile *rootfile;
+  string rootfn,newname;
+  TNamed *target;
+
+#if 0
+  //This filename input can be an alias
+  //
+  if( rootfn[0] == '@') {  // reference to an alias defined in ALIAS section
+    rootfn = extractAlias(rootfn.substr(1));
+    if( !rootfn.size()) return;
+  }
+#endif
+
+  if( outspec.find(":") != string::npos) {
+    vector<string> v_tokens;
+    Tokenize(outspec,v_tokens,":");
+
+    rootfn = v_tokens[0];
+    newname = v_tokens[1];
+  } else
+    rootfn = outspec;
+
+
+  rootfile = openRootFile(rootfn,"UPDATE");
+
+  if (rootfile) {
+    target = obj;
+    if( newname.size() ) {
+      cout<<"Writing object "<<newname<<" to file "<<rootfn<<endl;
+      target = (TNamed *)obj->Clone(newname.c_str());
+      //target->SetTitle(histo->GetTitle());
+    } else
+      cout<<"Writing object "<<obj->GetName()<<" to file "<<rootfn<<endl;
+
+    if (obj->InheritsFrom("TH1"))
+      ((TH1 *)target)->SetDirectory(rootfile);
+    target->Write();
+    //rootfile->Close();
+  }
+}                                                           // save2File
+
+//======================================================================
+
+void saveAs(TNamed *obj, const TString& path)
+{
+  TDirectory *savedir=gDirectory;
+
+  TObjArray *tokens = path.Tokenize(":");
+  TString filename = ((TObjString *)(*tokens)[0])->GetString();
+
+  if ((tokens->GetEntriesFast() != 2) ||
+      (!filename.EndsWith(".root"))     ) {
+    cerr << "ERROR: Expecting saveas=filename.root:objectname" << endl;
+    return;
+  }
+
+  TFile *file = openRootFile(std::string(filename.Data()), "UPDATE");
+  if (!file) {
+    cerr << " Couldn't open root file " << filename << endl;
+    exit(-1);
+  }
+  TString objname = ((TObjString *)(*tokens)[1])->GetString();
+
+  file->cd();
+
+  if (gl_verbose)
+    cout<<"Saving "<<TString(obj->GetName())<<" as "<<objname<<" to "<<filename<<endl;
+
+  obj->Write(objname);
+
+  file->Flush();
+
+  savedir->cd();
 }
